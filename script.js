3c7ccbadb8460c6d9a4c644a3d8718188fe373ae
Juergen <143478763+JuergenTJJ@users.noreply.github.com>
2026-09-25 10:59:38 +0800

    code error

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 const yesButton = document.getElementById("yesButton");
 const noButton = document.getElementById("noButton");
+const yesWrapper = document.getElementById("yesWrapper");
 
 let yesScale = 1;
 
 noButton.addEventListener("click", function () {
 
+    // Increase Yes button size
     yesScale = yesScale + 0.5;
 
     yesButton.style.transform = `scale(${yesScale})`;
+
+
+    // Remove previous shake
+    yesWrapper.classList.remove("shake");
+
+    // Restart the animation
+    void yesWrapper.offsetWidth;
+
+    // Add shake again
+    yesWrapper.classList.add("shake");
 
 });
